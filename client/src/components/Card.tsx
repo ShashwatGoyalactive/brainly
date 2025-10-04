bec70f72a59b60f4d716d35931595f41bcf16889
Shashwat <shashwatgoyal2004@gmail.com>
2025-10-04 20:39:43 +0530

    Card Component Updated

--- client/src/components/Card.tsx
+++ client/src/components/Card.tsx
@@ -1,42 +1,67 @@
-import { DeleteIcon } from '../icons/DeleteIcon';
-import { PlayIcon } from '../icons/PlayIcon';
-import { NotebookIcon  } from '../icons/NotebookIcon';
-import { TweetIcon } from '../icons/TweetIcon';
-import { ShareIcon } from './../icons/ShareIcon';
-
-
+import { DeleteIcon } from "../icons/DeleteIcon";
+import { PlayIcon } from "../icons/PlayIcon";
+import { TweetIcon } from "../icons/TweetIcon";
+import { ShareIcon } from "./../icons/ShareIcon";
 
 interface CardProps {
-  link?: string;
-  type: "tweet" | "youtube" | "article";
   title: string;
+  link: string;
+  type: "twitter" | "youtube";
   tags?: string[];
 }
 
 const IconType = {
-"youtube": <PlayIcon/>,
-"article" : <NotebookIcon/>,
-"tweet": <TweetIcon/>
-}
+  youtube: <PlayIcon />,
+  twitter: <TweetIcon />,
+};
 
-
-export const Card = (props :CardProps) => {
+export const Card = (props: CardProps) => {
   return (
     <div>
       <div
-        className={` p-4 bg-white  rounded-md border border-slate-100   outline-slate-200 max-w-72 `}
+        className={` p-4 bg-white  rounded-md border border-slate-100   outline-slate-200 min-h-48 max-w-72 `}
       >
         <div className="flex justify-between  ">
           <div className="flex items-center ">
             <div className="text-gray-500 pr-2">{IconType[props.type]}</div>
-            <h1 className='text-sm font-bold'>{props.title}</h1>
+            <h1 className="text-sm font-bold">{props.title}</h1>
           </div>
-          <div className="flex gap-4 items-center text-gray-500 ">
-            <ShareIcon />
+          <div className="flex gap-4 items-center text-gray-500 cursor-pointer">
+            <a
+              href={props.link.replace("watch", "embed").replace("?v=", "/")}
+              target="_blank"
+            >
+              <ShareIcon />
+            </a>
             <DeleteIcon />
           </div>
         </div>
+
+        {props.type === "youtube" ? (
+          <iframe
+            className="w-full pt-4"
+            src={props.link.replace("watch", "embed").replace("?v=", "/")}
+            title="YouTube video player"
+            frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            referrerPolicy="strict-origin-when-cross-origin"
+            allowFullScreen
+          ></iframe>
+        ) : (
+          <blockquote className="twitter-tweet">
+            <p lang="en" dir="ltr">
+              I’m launching a Fellowship{" "}
+              <a href="https://t.co/4pkaJHoLNE">pic.twitter.com/4pkaJHoLNE</a>
+            </p>
+            &mdash; Harkirat Singh (@kirat_tw){" "}
+            <a href="https://twitter.com/kirat_tw/status/1929805164963061811?ref_src=twsrc%5Etfw">
+              June 3, 2025
+            </a>
+          </blockquote>
+        )}
       </div>
+
+      {/* TODO : Implement the tag rendering logic here  */}
     </div>
   );
 };
